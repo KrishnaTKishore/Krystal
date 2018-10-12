9b735958f0c5cc8f85cee5d115477a9a4f7595aa
Student <Student@DESKTOP-JOKA8G8>
2018-10-12 09:59:59 +0530

    all commited

---
+++ Firsttrial/Firsttrial/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Firsttrial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=2, b=3,c;
+            c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
