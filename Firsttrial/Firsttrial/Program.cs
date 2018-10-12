631135a5b72d2e3868b50098f41fb8effa799a0d
Student <Student@DESKTOP-JOKA8G8>
2018-10-12 10:14:18 +0530

    second commit

--- Firsttrial/Firsttrial/Program.cs
+++ Firsttrial/Firsttrial/Program.cs
@@ -7,7 +7,10 @@ namespace Firsttrial
     {
         static void Main(string[] args)
         {
-            int a=2, b=3,c;
+            int a, b,c;
+            Console.WriteLine("Enter the value of a and b");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
             c = a + b;
             Console.WriteLine(c);
             Console.ReadKey();
